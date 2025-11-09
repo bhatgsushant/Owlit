8a51df03896eadd84c1c6ae4a003640eb7e89701
bhatgsushant <96242919+bhatgsushant@users.noreply.github.com>
2025-11-09 22:29:13 +0000

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -61,7 +61,7 @@ const LoginPage = () => {
               </svg>
             </div>
             <span className="text-3xl font-extrabold text-white">
-              ReceitAI
+              Owlit
             </span>
           </div>
           <h1 className="text-3xl font-bold text-white">Welcome Back</h1>
